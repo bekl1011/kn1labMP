9d69ddd75a2cef22799b8ca61325e5b0e16a67a9
bekl1011 <106782449+bekl1011@users.noreply.github.com>
2024-03-01 01:22:20 +0100

    Edit text

--- versuch4/scripts/runTopo.sh
+++ versuch4/scripts/runTopo.sh
@@ -8,6 +8,6 @@ sudo /usr/bin/python3 /home/labrat/kn1labMP/versuch4/scripts/mininet_2.py
 elif [ $1 -eq 3 ]
 sudo /usr/bin/python3 /home/labrat/kn1labMP/versuch4/scripts/mininet_3.py
 else
-echo "Please give a number from 1 to 3 to elect the mininet script you want to execute!"
+echo "Please give a number from 1 to 3 to select the mininet script you want to execute!"
 fi
 
